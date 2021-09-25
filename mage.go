77ce9606b60b7be5a05b7e2429f5bcedac8068cf
mar <gim.marcello@gmail.com>
2021-09-25 22:48:09 +0200

    added: basic mage file

---
+++ mage.go
@@ -0,0 +1,19 @@
+//+build mage
+
+package main
+
+func Build() error {
+	return nil
+}
+
+func Clean() error {
+	return nil
+}
+
+func Install() error {
+	return nil
+}
+
+func Uninstall() error {
+	return nil
+}
