26fc0ae61c25a1a67ed2b89a0f49f15a675ee992
mar <gim.marcello@gmail.com>
2021-10-02 18:41:26 +0200

    added: second part of the project skeleton

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Abathargh/ff-card-be
 
 go 1.16
+
+require (
+	github.com/go-redis/redis/v8 v8.11.3
+	github.com/gorilla/mux v1.8.0
+)
--- pkg/matchmaking/server.go
+++ pkg/matchmaking/server.go
@@ -1,15 +1,10 @@
 package matchmaking
 
-import (
-	"fmt"
-	"net"
-)
-
 type GameServer struct {
 }
 
 func NewServer(serverPort int) {
-	address := fmt.Sprintf("0.0.0.0:%d", serverPort)
-	server, err := net.Listen("tcp", address)
+	//address := fmt.Sprintf("0.0.0.0:%d", serverPort)
+	//server, err := net.Listen("tcp", address)
 
 }
